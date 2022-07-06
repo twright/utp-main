853c06392a1e3ad3e9605a1fa9902a779cf6e486
Thomas Wright <thomas.d.wright@york.ac.uk>
2022-07-07 00:56:39 +0100

    Prove correctness of tick tock traces for DoT.

--- theories/tockcircus/tcircus_reftrace_calculation.thy
+++ theories/tockcircus/tcircus_reftrace_calculation.thy
@@ -144,7 +144,7 @@ next
   qed
 qed
 
-lemma tockSequenceTockify: "tockSequence UNIV t \<Longrightarrow> t \<in> range tockify"
+lemma tockSequenceTockify: "tockSequence X t \<Longrightarrow> t \<in> range tockify"
 proof (induct t rule: tockSequence.induct)
   case (tockSequence0)
   then show ?case
@@ -301,5 +301,88 @@ next
   show "tttracesTI (Wait \<guillemotleft>n\<guillemotright>) = ?TI"
     using tttracesTIWait by blast
 qed
+
+subsection \<open> Do \<close>
+
+lemma tocksTockifyFinal: "t \<in> tocks (- {refevt e})
+  \<Longrightarrow> \<exists>s. tockify (t @ [Evt e]) = s @ [oevt e] \<and> tockSequence (- {refevt e}) s \<and> (\<exists>X. \<not> refevt e \<in> X)"
+  by (metis insert_absorb insert_not_empty tockSeqTocks tockify.simps(1) tockify.simps(3) tockifyAppend)
+
+lemma tocksTockifyFinalTock:
+    "tockSequence (- {refevt e}) t
+ \<Longrightarrow> \<not> refevt e \<in> X
+ \<Longrightarrow> \<exists>ta. t @ [oevt e] = tockify ta \<and> (\<exists>tb. tb \<in> tocks (- {refevt e}) \<and> (\<exists>x. ta = tb @ x \<and> x \<subseteq>\<^sub>t [Evt e]))"
+proof -
+  assume 1: "tockSequence (- {refevt e}) t"
+  assume 2: "\<not> refevt e \<in> X"
+  obtain "tc" where 3: "t = tockify tc"
+    by (meson tockSequenceTockify "1" image_iff)
+  have 3: "t @ [oevt e] = tockify(tc @ [Evt e])"
+    by (simp add: "3" tockifyAppend)
+  have 4: "tc \<in> tocks (- {refevt e})"
+    by (metis "1" "3" append_eq_append_conv tockSeqTocks tockify.simps(1) tockify.simps(3) tockifyAppend)
+  show ?thesis
+    using "3" "4" tock_ord_refl by auto
+qed
+
+lemma tttracesDo: "tttraces (DoT \<guillemotleft>e\<guillemotright> :: '\<phi> ttcsp)
+                 = ( {t . tockSequence (-{refevt e}) t}
+                   \<union> { t@[oevt e]
+                     | t X . tockSequence (-{refevt e}) t })
+                 \<union> {t@[oref X] | t X . tockSequence (-{refevt e}) t
+                              \<and> X \<subseteq> (-{refevt e})}
+                 \<union> { t@[oevt e, otick]
+                   | t X . tockSequence (-{refevt e}) t
+                         \<and> X \<subseteq> (-{refevt e})}" (is "?l = ?FE \<union> ?FR \<union> ?TI")
+proof (rule tttracesCalc)
+  show "tttracesFE (DoT \<guillemotleft>e\<guillemotright> :: '\<phi> ttcsp) = ?FE"
+    apply(rdes_simp)
+    apply(rel_auto)
+    using tockSeqTocks apply blast
+    apply (meson tocksTockifyFinal)
+    apply (meson tocksTockifyFinal)
+    apply (metis image_iff rfnil_mem_dest tockSeqTocks tockSequenceTockify)
+    by (meson empty_iff tocksTockifyFinalTock)
+next
+  show "tttracesFR (DoT \<guillemotleft>e\<guillemotright> :: '\<phi> ttcsp) = ?FR"
+    apply(rdes_simp)
+    apply(rel_auto)
+    using tockSeqTocks apply blast
+    by (metis image_iff tockSeqTocks tockSequenceTockify)
+next
+  show "tttracesTI (DoT \<guillemotleft>e\<guillemotright> :: '\<phi> ttcsp) = ?TI"
+    by (rdes_simp; rel_auto; auto simp add: tocksTockifyFinal tocksTockifyFinalTock)
+qed
+
+subsection \<open> Sequential composition \<close>
+
+lemma
+"true \<sqsubseteq> Q"
+  by (rel_simp)
+
+lemma tracesFERefine: "P \<sqsubseteq> Q \<Longrightarrow> tttracesFE Q \<subseteq> tttracesFE P"
+  apply(rdes_simp)
+  apply(rel_simp)
+  apply(simp add: tockifyEq)
+  by blast
+
+lemma tockifySetEq: "({tockify t| t. P} = {tockify t| t. Q}) = ({t. P} = {t. Q})"
+  by (auto)
+
+lemma "tttracesFE P \<subseteq> tttracesFE (P ;; Q)"
+  oops
+
+lemma "tttracesTI (P ;; Q) = {t@s| t s. t@[otick] \<in> tttracesTI P \<and> s \<in> tttracesTI Q}"
+  apply(rdes_simp)
+  apply(rel_auto)
+  oops
+  
+
+lemma "tttraces (P ;; Q) = tttracesFE P \<union> tttracesFR Q
+    \<union> {t@s| t s. t@[otick] \<in> tttracesTI P \<and> s \<in> tttraces Q}"
+  apply(rdes_simp)
+  apply(rel_auto)
+  apply(simp_all add: tockifyEq)
+  oops
 
 end
